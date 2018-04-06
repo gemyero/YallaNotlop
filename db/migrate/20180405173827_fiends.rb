09d45a83f8d04be37cefff2142513ee962eb5508
Mohamed Gamal <m.gemyero91@gmail.com>
2018-04-06 15:08:55 +0200

    remove id key from friends table

--- db/migrate/20180405173827_fiends.rb
+++ db/migrate/20180405173827_fiends.rb
@@ -1,6 +1,6 @@
 class Fiends < ActiveRecord::Migration[5.1]
   def change
-    create_table :friends do |t|
+    create_table :friends, id: false do |t|
       t.integer "user_id"
       t.integer "friend_id"
     end
